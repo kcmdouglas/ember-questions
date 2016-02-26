47af015d9cff7a226eb34116d526dafa37295b27
Kassidy Douglas <kcmdouglas@gmail.com>
2016-02-26 15:21:20 -0800

    Add links for categories.

--- app/router.js
+++ app/router.js
@@ -10,6 +10,12 @@ Router.map(function() {
   this.route('new-question');
   this.route('admin');
   this.route('question', {path: '/question/:question_id'});
+  this.route('backpacking');
+  this.route('camping');
+  this.route('hiking');
+  this.route('astronomy');
+  this.route('plant-identification');
+  this.route('bigfoot');
 });
 
 export default Router;
